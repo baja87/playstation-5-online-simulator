c684ca3f277bd4146d96495b924a2b83aed5e98d
ComputerEngineer1007 <93746222+baja87@users.noreply.github.com>
2024-04-22 03:53:46 -0400

    44566

--- src/parts/Social.js
+++ src/parts/Social.js
@@ -1,11 +1,8 @@
 import React from "react";
 import { ReactComponent as IconTwitter } from "../assets/icon-twitter.svg";
-// import { ReactComponent as IconCodepen } from "../assets/icon-codepen.svg";
-// import { ReactComponent as IconGithub } from "../assets/icon-github.svg";
-import { ReactComponent as IconFacebook } from "../assets/icon-facebook.svg";
-import { ReactComponent as IconLinkedin } from "../assets/icon-linkedin.svg";
-// import { ReactComponent as IconYoutube } from "../assets/icon-youtube.svg";
-// import { ReactComponent as IconMedium } from "../assets/icon-medium.svg";
+
+
+
 
 const Social = () => {
   return (
@@ -22,43 +19,13 @@ const Social = () => {
       <div className="ps5-btn-group">
         <a
           target="_blank"
-          rel="noopener"
+          rel="noreferrer"
           title="Twitter"
           className="ps5-btn ps5-btn-lg ps5-btn-mono ps5-btn-primary"
           href="https://twitter.com/intent/retweet?tweet_id=1334921677340372998"
           // href="https://twitter.com/intent/follow?screen_name=edalgrin&tw_p=followbutton"
         >
           <IconTwitter />
-        </a>
-
-        <a
-          target="_blank"
-          rel="noopener"
-          title="Facebook"
-          className="ps5-btn ps5-btn-lg ps5-btn-mono ps5-btn-primary"
-          href="https://www.facebook.com/sharer/sharer.php?kid_directed_site=0&sdk=joey&u=https://ps5.edalgrin.com/&display=popup&ref=plugin&src=share_button"
-        >
-          <IconFacebook />
-        </a>
-
-        <a
-          target="_blank"
-          rel="noopener"
-          title="Linkedin"
-          className="ps5-btn ps5-btn-lg ps5-btn-mono ps5-btn-primary"
-          href="https://www.linkedin.com/shareArticle?mini=true&url=https://ps5.edalgrin.com/&title=Playstaion%205%20Online%20Simulator&summary=This%20Swebsite%20Sis%20Sa%20Sstudy%20Sproject%20Sabout%20SPlayStation%20S5%20SUser%20SExperience"
-        >
-          <IconLinkedin />
-        </a>
-
-        <a
-          href="https://edalgrin.com/#active"
-          target="_blank"
-          rel="noopener"
-          title="Website"
-          className="ps5-btn ps5-btn-lg ps5-btn-mono ps5-btn-primary"
-        >
-          <img src="https://edalgrin.com/logo192.png" alt="" />
         </a>
       </div>
       <h2>Thank you! 💙</h2>
